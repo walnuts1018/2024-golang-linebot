70fb42b3f12bbefc07055f2d20a5c3a9bd8e67a0
walnuts1018 <r.juglans.1018@gmail.com>
2024-04-14 04:25:28 +0900

    fix

--- common/line.go
+++ common/line.go
@@ -15,53 +15,54 @@ func NewRouter(cfg config.Config) (*gin.Engine, error) {
 		gin.SetMode(gin.ReleaseMode)
 	}
 	r := gin.Default()
-	r.GET("/healthz", func(c *gin.Context) {
-		c.String(200, "OK")
-	})
-
 	api, err := messaging_api.NewMessagingApiAPI(cfg.LineChannelSecret)
 	if err != nil {
 		return nil, err
 	}
 
-	r.POST("/callback", func(c *gin.Context) {
-		callback, err := webhook.ParseRequest(cfg.LineChannelSecret, c.Request)
-		if err != nil {
-			slog.Error(fmt.Sprintf("Failed to parse request: %v", err))
-			c.JSON(500, gin.H{"error": "Failed to parse request"})
-			return
-		}
+	proxy := r.Group("/proxy/8080")
+	{
+		proxy.GET("/healthz", func(c *gin.Context) {
+			c.String(200, "OK")
+		})
+		proxy.POST("/callback", func(c *gin.Context) {
+			callback, err := webhook.ParseRequest(cfg.LineChannelSecret, c.Request)
+			if err != nil {
+				slog.Error(fmt.Sprintf("Failed to parse request: %v", err))
+				c.JSON(500, gin.H{"error": "Failed to parse request"})
+				return
+			}
 
-		for _, event := range callback.Events {
-			slog.Info(fmt.Sprintf("Event: %v", event))
+			for _, event := range callback.Events {
+				slog.Info(fmt.Sprintf("Event: %v", event))
 
-			switch e := event.(type) {
-			case *webhook.MessageEvent:
-				switch message := e.Message.(type) {
-				case *webhook.TextMessageContent:
-					if _, err := api.ReplyMessage(
-						&messaging_api.ReplyMessageRequest{
-							ReplyToken: e.ReplyToken,
-							Messages: []messaging_api.MessageInterface{
-								messaging_api.TextMessage{
-									Text: message.Text,
+				switch e := event.(type) {
+				case *webhook.MessageEvent:
+					switch message := e.Message.(type) {
+					case *webhook.TextMessageContent:
+						if _, err := api.ReplyMessage(
+							&messaging_api.ReplyMessageRequest{
+								ReplyToken: e.ReplyToken,
+								Messages: []messaging_api.MessageInterface{
+									messaging_api.TextMessage{
+										Text: message.Text,
+									},
 								},
 							},
-						},
-					); err != nil {
-						slog.Error(fmt.Sprintf("Failed to reply message: %v", err))
-						c.JSON(500, gin.H{"error": "Failed to reply message"})
-						return
+						); err != nil {
+							slog.Error(fmt.Sprintf("Failed to reply message: %v", err))
+							c.JSON(500, gin.H{"error": "Failed to reply message"})
+							return
+						}
+
+						slog.Info(fmt.Sprintf("Replied message: %v", message.Text))
 					}
 
-					slog.Info(fmt.Sprintf("Replied message: %v", message.Text))
 				}
-
 			}
-		}
 
-		c.JSON(200, gin.H{"message": "OK"})
-	})
-
+			c.JSON(200, gin.H{"message": "OK"})
+		})
+	}
 	return r, nil
 }
